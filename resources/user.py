d0806072ec2a39a7b519e41b4f029bca2365631c
Haruna Ogweda <hogweda@gmail.com>
2023-08-03 16:26:12 -0500

    add settings

--- resources/user.py
+++ resources/user.py
@@ -9,18 +9,12 @@ from db import db
 from blocklist import BLOCKLIST
 from models import UserModel
 from schemas import UserSchema, UserRegisterSchema
+from flask import current_app
+from tasks import send_user_registration_email
 
 blp = Blueprint("Users", "users", description="Operations on users")
 
-def send_simple_message(to, subject, body):
-    domain = os.getenv("MAILGUN_DOMAIN")
-    return requests.post(
-		f"https://api.mailgun.net/v3/{domain}/messages",
-		auth=("api", os.getenv("MAILGUN_API_KEY")),
-		data={"from": f"Haruna Ogweda <mailgun@Y{domain}>",
-			"to": [to],
-			"subject": subject,
-			"text": body})
+
 
 @blp.route("/register")
 class UserRegister(MethodView):
@@ -42,11 +36,7 @@ class UserRegister(MethodView):
         db.session.add(user)
         db.session.commit()
 
-        send_simple_message(
-            to=user.email,
-            subject="Successfully signed up",
-            body=f"Hi {user.username}! You have successfully signed up to the Stores Rest API"
-        )
+        current_app.queue.enqueue(send_user_registration_email, user.email, user.username)
 
         return {"message": "User created successfully."}, 201
     
